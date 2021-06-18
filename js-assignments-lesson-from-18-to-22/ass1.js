f1c8e5e8b8f47e7c3fd24d8cf4e5fde7613f5af6
mohammedtoumi007 <medtoumi007@gmail.com>
2021-06-18 16:38:39 +0200

    lesson 3

---
+++ js-assignments-lesson-from-18-to-22/ass1.js
@@ -0,0 +1,35 @@
+// Assignment One
+console.log(10 * 20 +(15 % 3) + 190 + 10 - 400); // 0
+
+// Assignment Two
+let num = 3;
+
+// Solution One
+console.log(num + num); // 6
+
+// Solution Two
+console.log(+num + +num); // 6
+
+// Soultion Three
+console.log(num += 3); // 6
+
+// Soultion Four
+console.log(++num - true); // 6
+
+// Solution Five
+console.log(num++ - true - false); // 6
+
+// Solution Six
+console.log(false + --num - true); // 6
+
+// Assignment Three
+let num2 = "10";
+
+// Solution One
+console.log(+num2 + +num2); // 20
+
+// Solution Two
+console.log(+num2++ + +num2++ - true); // 20
+
+// Soultion Three
+console.log(num2 + num2++ - true - true - true - true); // 20
